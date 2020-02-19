1d972e5d1a9a42afd82f6a191522b568fe873cb6
Loadingdotdotdot <sv072002@gmail.com>
2020-02-18 23:03:14 -0500

    upload project files

---
+++ softwaredev_2020_mobile/android/app/src/main/kotlin/softwaredev2020mobile/softwaredev_2020_mobile/MainActivity.kt
@@ -0,0 +1,12 @@
+package softwaredev2020mobile.softwaredev_2020_mobile
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
